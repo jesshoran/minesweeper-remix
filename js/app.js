3ee85f8ce4c5dab9d539cfb5a71797c088e1b808
lakergirl702 <jesshoran87@gmail.com>
2020-08-01 10:54:25 -0700

    build the board

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-console.log($)
+// console.log($)
+
+//one 9x9 board
+
+//need: timer, count number of mines left
+
+// if space seloected has a mine next to or adjascent, number of mines is counted and displayed
+// if mine selected, game over
+// if none, no number is displayed
+// if you think a mine is there, select something on the right and hide it
+
+//build board
+
+
+const $start = $("#start");
+const $board = $("#board");
+
+const makeBoard = () => {
+    $board.empty();
+    for (let i = 0; i < 81; i++) {
+        const $square = $('<div class="square">');
+        $board.append($square)
+    }
+}
+$start.on("click", makeBoard);
+
+
+//randomly set mines
+
+
+
+//
+
